17dad92f2a365e2747df30160e5bd0873547682c
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-14 16:34:04 +0000

    [add] milestone 3

--- script/script.js
+++ script/script.js
@@ -1,15 +1,50 @@
-import {contacts} from './db.js';
+import { contacts } from './db.js';
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-  data(){
-    return{
+  data() {
+    return {
       contacts,
-      activeId: 0
+      activeId: 0,
+      newMsg: '',
+      newText: {
+        date: '',
+        message: '',
+        status: ''
+      }
     }
   },
-  mounted(){
+  methods: {
+    newMessage() {
+      this.newText = { 
+      date: '01/01/1900',
+      message: this.newMsg,
+      status: 'sent'
+    }
+    this.contacts[this.activeId].messages.push(this.newText);
+    this.newMsg = ''
+    this.newText = { 
+      date: '',
+      message: '',
+      status: '',
+    }
+    },
+    answer(){
+      setTimeout(() =>{
+        
+        this.newAnswer = { 
+          date: '01/01/1900',
+          message: 'Messaggio dal BOT',
+          status: 'received',
+        }
+        this.contacts[this.activeId].messages.push(this.newAnswer);
+
+      }, 1000)
+     
+    }
+  },
+  mounted() {
     console.log(contacts);
 
   }
