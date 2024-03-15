df783547a55c822b5c3e210427db097eba0836da
davide-piarulli <davide.piarulli.1@gmail.com>
2024-03-15 11:14:44 +0000

    [add] milestone 4

--- script/script.js
+++ script/script.js
@@ -18,23 +18,23 @@ createApp({
   },
   methods: {
     newMessage() {
-      this.newText = { 
-      date: '01/01/1900',
-      message: this.newMsg,
-      status: 'sent'
-    }
-    this.contacts[this.activeId].messages.push(this.newText);
-    this.newMsg = ''
-    this.newText = { 
-      date: '',
-      message: '',
-      status: '',
-    }
+      this.newText = {
+        date: '01/01/1900',
+        message: this.newMsg,
+        status: 'sent'
+      }
+      this.contacts[this.activeId].messages.push(this.newText);
+      this.newMsg = ''
+      this.newText = {
+        date: '',
+        message: '',
+        status: '',
+      }
     },
-    answer(){
-      setTimeout(() =>{
-        
-        this.newAnswer = { 
+    answer() {
+      setTimeout(() => {
+
+        this.newAnswer = {
           date: '01/01/1900',
           message: 'Messaggio dal BOT',
           status: 'received',
@@ -42,13 +42,26 @@ createApp({
         this.contacts[this.activeId].messages.push(this.newAnswer);
 
       }, 1000)
-     
+
     }
   },
 
-  computed:{
-    nomiCercati(){
-      return contacts.contact.filter(message => message.name.toLowerCase().includes(this.nametoSearch.toLowerCase()))
+  computed: {
+    visibleContacts(){
+      return contacts.filter(contact => contact.visible)
+    },
+
+    searchContacts (){
+      contacts.forEach(contact => {
+        if (contact.name.toLowerCase().includes(this.nametoSearch.toLowerCase())) {
+          contact.visible = true
+        } else {
+          contact.visible = false
+  
+        }
+      })
+        
+      return contacts;
     }
   }
 }).mount('#app');
